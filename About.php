674f1cefb7d8279e4b7704e1c6404f457f281ee3
Yu Tzu Wu <abby8050@gmail.com>
2016-11-20 01:16:29 +0800

    Add files via upload

--- About.php
+++ About.php
@@ -12,7 +12,7 @@ class About implements Product{
 	private $msg = array (
 		'Information' => '<a id=\'TAFREE_A\' href=\'http://github.com/TAFree\'><b>TAFree</b></a>  is an online judge system for NTU CE Computer Programming course; it is open-source and welcome to keep hacking.<br>',
 		'Contributors' => 'Developers:<br>林照傑(edlin851206)<br>簡翰鈺(weavile182)<br>周琪雅(v06c)<br>張如嫻(JoyChal)<br>吳育姿(derailment)<br><br>Advisor:<br>吳軒竹(GGJason)',
-		'History' => 'TAFree緣起於台大土木系計算機程式課程，由於近年選課學生的增加，使得以往的批改作業方式及繁瑣的管理程序造成課程助教業務量增大，為了消弭人工作業的諸多不便與瑕疵，一個輔助助教的作業批改系統由此誕生。<br><br>
+		'History' => 'TAFree緣起於台大土木系計算機程式課程，由於近年選課學生的增加，使得以往的批改作業方式及繁瑣的管理程序造成課程助教業務量增大，為了消弭人工作業的諸多不便，一個輔助助教的作業批改系統由此誕生。<br><br>
 		104年暑假，周琪雅，簡翰鈺，林照傑，張如嫻，吳軒竹完成前端的互動功能TAFreeUI。<br>
 		105年學期，吳育姿延續TAFreeUI完成第一代平台TAFree1.0。<br>',
 		'Version' => 'TAFree1.0 is available for Google Chrome 54.0.2840.59'
