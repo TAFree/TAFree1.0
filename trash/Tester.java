884b2bd27ec7ee2f3a3bf1d487ffdf7e4868fea1
Abby <abby8050@gmail.com>
2016-11-27 03:18:25 +0800

    Add a java code with input need

--- trash/Tester.java
+++ trash/Tester.java
@@ -1,49 +1,51 @@
 import java.util.Scanner;
 
 public class Tester {
-    public static void main(String[] args) {
+ 
+   public static void main(String[] args) {
             
             Scanner scanner = new Scanner(System.in);
-            System.out.println('Please input a 2 character student code: ');
-            String in = nextLine();
-            if (in.length > 2) {
-                System.out.println('Input is more than two character!');
-                return;
+            System.out.print("Please input a 2 character student code: ");
+            String in = scanner.nextLine();
+            if (in.length() > 2) {
+                System.out.println("Input is more than two characters!");
+		return;
                 
             }
 
-            char s = in.indexOf(0);
-            String subject = '';
+            char s = in.charAt(0);
+            String subject = "";
             switch (s) {
                 case 'M': 
-                    subject = 'Medicine';
+                    subject = "Medicine";
                 break;
                 case 'C': 
-                    subject = 'Civil Engineering';
+                    subject = "Civil Engineering";
                 break;
                 case 'I': 
-                    subject = 'Computer Science & Information Engineering';
+                    subject = "Computer Science & Information Engineering";
                 break;
                 case 'L': 
-                    subject = 'Law';
+                    subject = "Law";
                 break;
                 case 'E': 
-                    subject = 'Electrical Engineering';
+                    subject = "Electrical Engineering";
                 break;
                 default:
-                    subject = 'Unknown';
+                    subject = "Unknown";
                 
             }
             
-            int y = in.indexOf(1);
-            String year = '';
+            int y = Integer.parseInt(String.valueOf(in.charAt(1)));
+            String year = "";
             int i = 0;
             while (i < y) {
-                year += '*';
+                year += "*";
+		i++;
             }
-            String subject
-            System.out.printf('The student majors in %s, and is in year: %s.', subject, year);
             
+            System.out.printf("The student majors in %s,\nand is in year: %s.\n", subject, year);
+	    
     }
 
 }
