02bea4e015830cf1a740be2a1b7105ea0e4e4f88
Yu Tzu Wu <abby8050@gmail.com>
2016-12-05 05:56:40 +0800

    Add files via upload

---
+++ classes/Context.php
@@ -0,0 +1,19 @@
+<?php
+namespace TAFree\classes;
+
+use TAFree\classes\IStrategy;
+
+class Context {
+	
+	private $strategy;
+	
+	public function __construct (IStrategy $strategy) {
+		$this->strategy = $strategy;
+	}
+	
+	public function algorithm () {
+		$this->strategy->algorithm();
+	}
+
+}
+?>
