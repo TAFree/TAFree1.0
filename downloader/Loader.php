f8eeeb2592a4d07c2a92149266760328cf56a828
root <root@TAFree>
2016-12-10 16:12:58 +0000

    Fix bug in Loader.php

--- downloader/Loader.php
+++ downloader/Loader.php
@@ -23,14 +23,11 @@ class Loader {
 		}
 	}
 	
-	public function download() {
-		header ('Content-Description: File Transfer');		
-		header ('Content-Type: application/octet-stream');		
+	public function download() {		
+		header ('Content-Type: text/html');		
 		header ('Content-Disposition: attachment; filename="' . basename($this->filename) . '"');		
 		header ('Expires: 0');		
-		header ('Cache-Control: must-revalidate');		
-		header ('Pragma: public');		
-		header ('Content-Length: ' . filesize($this->filename));		
+		header ('Pragma: no-cache');			
 		readfile($this->filename);
 		unlink($this->filename);
 		exit();
