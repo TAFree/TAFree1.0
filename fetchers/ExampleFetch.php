ae06c481e766f29760e6299aa152167d23e02073
Abby <abby8050@gmail.com>
2017-01-17 15:45:16 +0800

    Change ExampleFetch

--- fetchers/ExampleFetch.php
+++ fetchers/ExampleFetch.php
@@ -2,26 +2,45 @@
 namespace TAFree\fetchers;
 
 use TAFree\classes\Product;
+use TAFree\database\UniversalConnect;
 
 require_once('../composers/Autoloader.php');
 
 class ExampleFetch implements Product {
 	
 	private $examples;
+	private $hookup;
 
 	public function getContent() {
 		
 		$this->examples = '';
+			
+		try {
+			
+			$this->hookup = UniversalConnect::doConnect();	
+		
+			// Get judgescript and content from general table	
+			$stmt = $this->hookup->prepare('SELECT * FROM general');
+			$stmt->execute();
+			while($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+				$filename = $row['judgescript'];
+				$ext = '';
+				if (preg_match('/[^\.]+$/i', $filename, $matches)) {
+					$ext = $matches[0];
+				}
+				$this->examples .= '<h4>' . $filename . '</h4>';
+				$this->examples .= '<pre><code class=\'' . $ext . '\'>' . htmlspecialchars($row['content']) . '</code></pre><br>';
+			}
 
-		foreach (glob('../judge/*') as $filename) {
-			$ext = substr($filename, strrpos($filename, '.') + 1);
-			$handle = fopen($filename, 'r');
-			$contents = fread($handle, filesize($filename));
-			$this->examples .= '<pre><code class=\'' . $ext . '\'>' . '//' . $filename . '<br><br>' . htmlspecialchars($contents) . '</code></pre>';
-			fclose($handle);
+			$this->hookup = null;
+	
 		}
+		catch (\PDOException $e) {
+			echo 'Error: ' . $e->getMessage() . '<br>';
+		}	
 		
 		return $this->examples;
+	
 	}
 
 }
